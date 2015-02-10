200e2c1a86089fc86ea47aef8d41034236478ae1
Danial Sadiq Masood <danialsadiqmasood@hotmail.com>
2015-02-10 14:38:10 +0000

    patient and staff classes

---
+++ src/main/java/nuclibook/models/Patient.java
@@ -0,0 +1,55 @@
+package nuclibook.models;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
+@DatabaseTable(tableName = "patients")
+public class Patient {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField
+    private String name;
+
+    @DatabaseField
+    private String hospitalNumber;
+
+    @DatabaseField
+    private Date DOB;
+
+
+    public Patient() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHospitalNumber() {
+        return hospitalNumber;
+    }
+
+    public void setHospitalNumber(String hospitalNumber) {
+        this.hospitalNumber = hospitalNumber;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+}
