bbb052f7c36d0141cf3e5925a8101615ea42a94a
markormesher <me@markormesher.co.uk>
2015-02-10 18:21:54 +0000

    Added collection; removed useless 'remove' method; removed unneeded variable passing

--- src/main/java/nuclibook/server/HtmlRenderer.java
+++ src/main/java/nuclibook/server/HtmlRenderer.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,18 +15,22 @@ public class HtmlRenderer {
 	private String templateFile;
 
 	private HashMap<String, String> fields;
+	private HashMap<String, Collection> collections;
+
+	private String parsedHtml = "";
 
 	public HtmlRenderer(String templateFile) {
 		this.templateFile = templateFile;
 		fields = new HashMap<>();
+		collections = new HashMap<>();
 	}
 
 	public void setField(String key, String value) {
 		fields.put(key, value);
 	}
 
-	public void removeField(String key) {
-		fields.remove(key);
+	public void setCollection(String key, Collection collection) {
+		collections.put(key, collection);
 	}
 
 	private String readSimpleFile() {
@@ -50,40 +55,40 @@ public class HtmlRenderer {
 		}
 	}
 
-	private String parseConditionalFields(String html) {
-		Pattern fieldPattern = Pattern.compile("<!\\-\\-\\[if field: ([a-zA-Z0-9\\-]+)\\]\\-\\->(.*)<!\\-\\-\\[end if\\]\\-\\->");
-		Matcher fieldMatcher = fieldPattern.matcher(html);
+	private void parseConditionalFields() {
+		Pattern fieldPattern = Pattern.compile("<!\\-\\-\\[if field: ([a-zA-Z0-9\\-]+)\\]\\-\\->(.*?)<!\\-\\-\\[end if\\]\\-\\->");
+		Matcher fieldMatcher = fieldPattern.matcher(parsedHtml);
 		StringBuffer output = new StringBuffer();
 		while (fieldMatcher.find()) {
 			fieldMatcher.appendReplacement(output, getConditionalFieldValue(fieldMatcher.group(1), fieldMatcher.group(2)));
 		}
 		fieldMatcher.appendTail(output);
-		return output.toString();
+		parsedHtml = output.toString();
 	}
 
-	private String parseFields(String html) {
+	private void parseFields() {
 		Pattern fieldPattern = Pattern.compile("<!\\-\\-\\[field: ([a-zA-Z0-9\\-]+)\\]\\-\\->");
-		Matcher fieldMatcher = fieldPattern.matcher(html);
+		Matcher fieldMatcher = fieldPattern.matcher(parsedHtml);
 		StringBuffer output = new StringBuffer();
 		while (fieldMatcher.find()) {
 			fieldMatcher.appendReplacement(output, getFieldValue(fieldMatcher.group(1)));
 		}
 		fieldMatcher.appendTail(output);
-		return output.toString();
+		parsedHtml = output.toString();
 	}
 
 	private String getFieldValue(String key) {
-		return fields.containsKey(key) ? fields.get(key) : "";
+		return (fields.containsKey(key) && fields.get(key) != null) ? fields.get(key) : "";
 	}
 
 	private String getConditionalFieldValue(String key, String original) {
-		return fields.containsKey(key) ? original : "";
+		return (fields.containsKey(key) && fields.get(key) != null) ? original : "";
 	}
 
 	public String render() {
 		String html = readSimpleFile();
-		html = parseConditionalFields(html);
-		html = parseFields(html);
+		parseConditionalFields();
+		parseFields();
 		return html;
 	}
 
