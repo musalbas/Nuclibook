ecab408b9c1c19f2c7a156bced7f7269a56dcbb3
Danial Sadiq Masood <danialsadiqmasood@hotmail.com>
2015-02-10 11:50:17 +0000

    no message

--- src/main/java/nuclibook/server/SqlServerConnection.java
+++ src/main/java/nuclibook/server/SqlServerConnection.java
@@ -1,5 +1,7 @@
 package nuclibook.server;
 
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
 import nuclibook.constants.C;
 
 import java.sql.Connection;
@@ -18,8 +20,9 @@ public class SqlServerConnection {
 	public static Connection acquireConnection() {
 		if (connection == null) {
 			try {
-				Class.forName("com.mysql.jdbc.Driver").newInstance();
-				connection = DriverManager.getConnection(C.MYSQL_URI, C.MYSQL_USERNAME, C.MYSQL_PASSWORD);
+                ConnectionSource connectionSource = new JdbcConnectionSource(C.MYSQL_URI);
+                ((JdbcConnectionSource)connectionSource).setUsername(C.MYSQL_USERNAME);
+                ((JdbcConnectionSource)connectionSource).setPassword(C.MYSQL_PASSWORD);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
