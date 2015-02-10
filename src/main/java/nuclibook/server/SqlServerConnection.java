04a5f1d698847b08f2ceb1552a135d5fde821799
Mustafa <mus@musalbas.com>
2015-02-10 14:57:45 +0000

    Move init DB in try block

--- src/main/java/nuclibook/server/SqlServerConnection.java
+++ src/main/java/nuclibook/server/SqlServerConnection.java
@@ -25,11 +25,11 @@ public class SqlServerConnection {
 				connection = new JdbcConnectionSource(C.MYSQL_URI);
 				((JdbcConnectionSource) connection).setUsername(C.MYSQL_USERNAME);
 				((JdbcConnectionSource) connection).setPassword(C.MYSQL_PASSWORD);
+
+				initDB(connection);
 			} catch (Exception e) {
 				e.printStackTrace(); // TODO deal with exception
 			}
-
-			initDB(connection);
 		}
 
 		return connection;
