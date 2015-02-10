e73be8fb634596002e0c2b1673832b95d4ed4998
GEORGE RADUTA <k1332897@kcl.ac.uk>
2015-02-10 16:41:43 +0000

    Worked on Radiotracer.

--- src/main/resources/static/js/radiotracers.js
+++ src/main/resources/static/js/radiotracers.js
@@ -2,6 +2,14 @@
  * Created by GEORGE RADUTA on 10.02.2015.
  */
 
+
+
 function removeRadiotracer() {
-    console.log("Are");
+
 }
+
+function removeRadiotracerOkClick() {
+    console.log("Ok clicked");
+    document.getElementById("okRemoveRadiotracer").setAttribute("data-dismiss","modal");
+}
+
