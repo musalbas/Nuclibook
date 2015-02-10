792783fc6b26c542d048c2d5836365d139ee89de
Mustafa <mus@musalbas.com>
2015-02-10 11:12:21 +0000

    Update User model

--- src/main/java/nuclibook/models/User.java
+++ src/main/java/nuclibook/models/User.java
@@ -1,6 +1,19 @@
+package nuclibook.models;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "users")
 public class User {
 
+    @DatabaseField(id = true)
+    private String id;
+
+    @DatabaseField
     private String name;
+
+    @DatabaseField
+    private String password;
 
     public String getName() {
         return name;
