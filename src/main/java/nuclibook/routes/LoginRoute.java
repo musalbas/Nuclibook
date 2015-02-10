6ebf26aba1c56aaad70c4e3db68bf5ef794abc61
markormesher <me@markormesher.co.uk>
2015-02-10 12:25:43 +0000

    Implemented different routes for login get/post in same class; dummy login form with post fields

--- src/main/java/nuclibook/routes/LoginRoute.java
+++ src/main/java/nuclibook/routes/LoginRoute.java
@@ -12,6 +12,19 @@ public class LoginRoute extends DefaultRoute {
 
 	@Override
 	public Object handle(Request request, Response response) throws Exception {
-		return "";
+		if (getRequestType() == RequestType.GET) {
+			return handleGet(request, response);
+		} else {
+			return handlePost(request, response);
+		}
+	}
+
+	public Object handleGet(Request request, Response response) throws Exception {
+		// TODO: implement actual front end login form
+		return "<form action=\"/login\" method=\"post\"><input type=\"text\" name=\"username\" /><br /><input type=\"text\" name=\"password\" /><br /><input type=\"Submit\" value=\"Login\" /></form>";
+	}
+
+	public Object handlePost(Request request, Response response) throws Exception {
+		return "POSTED: " + request.queryParams("username") + " / " + request.queryParams("password");
 	}
 }
