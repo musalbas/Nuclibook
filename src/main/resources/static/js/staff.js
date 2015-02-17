a848455107280b090da4574b71012119c567b762
markormesher <me@markormesher.co.uk>
2015-02-17 15:42:00 +0000

    Better staff validation

--- src/main/resources/static/js/staff.js
+++ src/main/resources/static/js/staff.js
@@ -1,11 +1,49 @@
-var validateCreateForm = function(formObject) {
+// TODO: use the toastr library for errors, instead of alerts
+
+var validateEditForm = function(formObject) {
+	var error = false;
+
 	// check name
-	if (formObject.username.length < 1) {
-		alert("You did not enter a valid username");
-		return false;
+	if (formObject.name.length < 1) {
+		alert("You did not enter a valid name");
+		error = true;
 	}
 
-	// TODO: check password length and match
+	// check username
+	if (formObject.username.length < 1) {
+		alert("You did not enter a valid username");
+		error = true;
+	}
 
-	return true;
+	return !error;
+};
+
+var validateCreateForm = function(formObject) {
+	var error = false;
+
+	// check name
+	if (formObject.name.length < 1) {
+		alert("You did not enter a valid name");
+		error = true;
+	}
+
+	// check username
+	if (formObject.username.length < 1) {
+		alert("You did not enter a valid username");
+		error = true;
+	}
+
+	// check password length
+	if (formObject.password.length < 4) {
+		alert("Your password must be at least 4 characters long");
+		error = true;
+	}
+
+	// check password match
+	if (formObject.password != formObject.password_check) {
+		alert("Your passwords did not match");
+		error = true;
+	}
+
+	return !error;
 };
