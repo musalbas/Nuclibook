08c9b5c2d8c790d565213fa6ec276778ea0ad588
Mark Ormesher <me@markormesher.co.uk>
2015-02-20 23:41:45 +0000

    Removed unused check - will replace once roles are implemented

--- src/main/resources/static/js/staffrole.js
+++ src/main/resources/static/js/staffrole.js
@@ -1,15 +1,7 @@
-/**
- * Created by Navi on 20/02/2015.
- */
-
-
-
 var validateCreateForm = function (formObject) {
     var error = false;
-
-    if (formObject.permission.length < 1) {
-        toastr.error("Please assign permission to role");
-        error = true;
-    }
-        return !error
-    }
+    
+    // TODO
+    
+    return !error;
+}
