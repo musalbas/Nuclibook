220c8cbe1c5c44d67e27b4743f5ae13c91ce6a95
GEORGE RADUTA <k1332897@kcl.ac.uk>
2015-02-24 14:50:36 +0000

    Trim added to Mark's Form Validations.

--- src/main/resources/static/js/staff.js
+++ src/main/resources/static/js/staff.js
@@ -2,13 +2,13 @@ var validateEditForm = function (formObject) {
 	var error = false;
 
 	// check name
-	if (formObject.name.length < 1) {
+	if (formObject.name.trim().length < 1) {
 		toastr.error("You did not enter a valid name");
 		error = true;
 	}
 
 	// check username
-	if (formObject.username.length < 1) {
+	if (formObject.username.trim().length < 1) {
 		toastr.error("You did not enter a valid username");
 		error = true;
 	}
@@ -20,19 +20,19 @@ var validateCreateForm = function (formObject) {
 	var error = false;
 
 	// check name
-	if (formObject.name.length < 1) {
+	if (formObject.name.trim().length < 1) {
 		toastr.error("You did not enter a valid name");
 		error = true;
 	}
 
 	// check username
-	if (formObject.username.length < 1) {
+	if (formObject.username.trim().length < 1) {
 		toastr.error("You did not enter a valid username");
 		error = true;
 	}
 
 	// check password length
-	if (formObject.password.length < 4) {
+	if (formObject.password.trim().length < 4) {
 		toastr.error("Your password must be at least 4 characters long");
 		error = true;
 	}
