b043713db862ca81f1a05cdda1f01d8b71a82d94
GEORGE RADUTA <k1332897@kcl.ac.uk>
2015-02-24 14:46:53 +0000

    Basic form validations added. More to follow

--- src/main/resources/static/js/therapies.js
+++ src/main/resources/static/js/therapies.js
@@ -1,7 +1,30 @@
 var validateCreateForm = function (formObject) {
     var error = false;
 
-	// TODO
+    var therapyNameString = formObject["name"];
+    var therapyDuration = formObject["default-duration-mins"];
+    var therapyMedicineRequired = formObject["medicine-required-name"];
+    var therapyMedicineDose = formObject["medicine-dose"];
+    var therapyCamera = formObject["camera-type-label"];
+
+    if (therapyNameString.trim().length < 1) {
+        toastr.error("You did not enter a valid name for the therapy");
+        error = true;
+    }
+
+    if ($.isNumeric(therapyDuration) == false) {
+        toastr.error("You did not enter a valid value for the duration of the therapy")
+        error = true;
+    }
+
+    //TODO therapyMedicineRequired. Ask the client;
+
+    if (therapyMedicineDose.trim().length < 1) {
+        toastr.error("You did not enter a valid value for the medicine dose");
+        error = true;
+    }
+
+    //TODO therapyCamera. Ask the client.
 
     return !error;
 };
