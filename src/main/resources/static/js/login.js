e21c763b5bebfa774d0c94375cdd478bc345c99c
Fares Alaboud <fares.alaboud@gmail.com>
2015-02-25 10:25:36 +0000

    [fixed]
    - Panel was very far down the page (about an inch of the bottom of the screen)
    - There was a slight horizontal screen (i.e. horizontal scroll bar showing) for some reason
    - The spacing between the red error box and the login text was a bit too big
    - ^ Same for the "Hello XYZ" and the form instruction
    - Put "Hello XYZ" inside on the alert boxes, but with `info` class instead of `danger`
    - The bug with having to click button several times is fixed

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,22 +2,22 @@
  * Created by Fares Alaboud on 24/02/2015.
  */
 
-function keepFocus(field) {
-    var focusedElement = document.getElementById(field);
+function keepFocus() {
+    var focusedElement = $(stage == 2 ? '#password' : '#username');
 
     focusedElement.focus();
 
-    focusedElement.onblur = function () {
+    focusedElement.unbind("blur").blur(function () {
         setTimeout(function () {
             focusedElement.focus();
-        });
-    };
-}
-
-function submitForm() {
-    var form = document.getElementById("login-form");
-
-    document.getElementById("login-continue-button").addEventListener("click", function () {
-        form.submit();
+        }, 0);
     });
 }
+
+$(document).ready(function() {
+   keepFocus();
+});
+
+$(window).focus(function() {
+   keepFocus();
+});
