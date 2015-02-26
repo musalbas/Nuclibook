73a078b8800c85777b5943df17fccb12151a5709
GEORGE RADUTA <k1332897@kcl.ac.uk>
2015-02-26 17:27:00 +0000

    Medicines done
    Checks;
    
    Name field not to be empty  and not to exceed 64 characters(not trimmed)
    Order time to be positive and not exceed an integer.

--- src/main/resources/static/js/medicines.js
+++ src/main/resources/static/js/medicines.js
@@ -2,9 +2,13 @@
 
 var validateCreateForm = function (formObject) {
     var error = false;
-    // check name
+    // check name to be in interval [1,64]
     if (formObject.name.trim().length < 1) {
-        toastr.error("You did not enter a valid name");
+        toastr.error("You did not enter a valid name.");
+        error = true;
+    }
+    if (formObject.name.length > 64) {
+        toastr.error("Name of the medicine should not exceed 64 characters.");
         error = true;
     }
 
@@ -13,6 +17,11 @@ var validateCreateForm = function (formObject) {
     if ($.isNumeric(number) == false) {
         toastr.error("You did not enter a valid order time. Expecting a number.");
 		error = true;
+    } else {
+        if (number < '0' || number > '2147483647') {
+            toastr.error("Order time should be a positive number smaller than 2,147,483,647");
+            error = true;
+        }
     }
 
     return !error;
@@ -20,18 +29,27 @@ var validateCreateForm = function (formObject) {
 
 var validateEditForm = function (formObject) {
 	var error = false;
-	// check name
-	if (formObject.name.trim().length < 1) {
-		toastr.error("You did not enter a valid name");
-		error = true;
-	}
+    // check name to be in interval [1,64]
+    if (formObject.name.trim().length < 1) {
+        toastr.error("You did not enter a valid name.");
+        error = true;
+    }
+    if (formObject.name.length > 64) {
+        toastr.error("Name of the medicine should not exceed 64 characters.");
+        error = true;
+    }
 
-	// check if the user inserted a number
-	var number = formObject['order-time'];
-	if ($.isNumeric(number) == false) {
-		toastr.error("You did not enter a valid order time. Expecting a number");
-		error = true;
-	}
+    // check if the user inserted an integer
+    var number = formObject['order-time'];
+    if ($.isNumeric(number) == false) {
+        toastr.error("You did not enter a valid order time. Expecting a number.");
+        error = true;
+    } else {
+        if (number < '0' || number > '2147483647') {
+            toastr.error("Order time should be a positive number smaller than 2,147,483,647");
+            error = true;
+        }
+    }
 
-	return !error;
+    return !error;
 };
