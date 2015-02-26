9465ff6f2e4ac87545e631bf5852276835459edf
GEORGE RADUTA <k1332897@kcl.ac.uk>
2015-02-26 17:03:32 +0000

    Cameras and Cameras-Types validation.
    Checks done :  Input fields to be in range of [1,32]

--- src/main/resources/static/js/cameras.js
+++ src/main/resources/static/js/cameras.js
@@ -6,6 +6,10 @@ var validateEditForm = function (formObject) {
         toastr.error("You did not enter a room number");
         error = true;
     }
+    if (formObject['room-number'].trim().length > 32) {
+        toastr.error("Room number should not exceed 32 characters.");
+        error = true;
+    }
 
     return !error;
 };
@@ -16,6 +20,10 @@ var validateCreateForm = function (formObject) {
 	// check room number
     if (formObject['room-number'].trim().length < 1) {
         toastr.error("You did not enter a room number");
+        error = true;
+    }
+    if (formObject['room-number'].trim().length > 32) {
+        toastr.error("Room number should not exceed 32 characters.");
         error = true;
     }
 
