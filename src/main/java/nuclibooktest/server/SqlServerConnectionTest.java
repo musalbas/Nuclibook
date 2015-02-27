8f0cec5e5562cdcee911868d72a84e7baa21c8d8
Mark Ormesher <me@markormesher.co.uk>
2015-02-27 11:43:47 +0000

    Fixed a few merge issues

--- src/main/java/nuclibooktest/server/SqlServerConnectionTest.java
+++ src/main/java/nuclibooktest/server/SqlServerConnectionTest.java
@@ -3,13 +3,10 @@ package nuclibooktest.server;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.support.ConnectionSource;
-import nuclibook.models.Staff;
 import nuclibook.models.Tracer;
 import nuclibooktest.test_utils.TestSqlServerConnection;
 import org.junit.AfterClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import java.sql.SQLException;
 
@@ -17,18 +14,18 @@ import static junit.framework.TestCase.assertTrue;
 
 public class SqlServerConnectionTest {
 
-    @Test
-    public void testAcquireConnection() throws SQLException {
-        ConnectionSource connection = TestSqlServerConnection.acquireConnection();
-        assertTrue("SQL connection was not created", connection instanceof ConnectionSource);
+	@Test
+	public void testAcquireConnection() throws SQLException {
+		ConnectionSource connection = TestSqlServerConnection.acquireConnection();
+		assertTrue("SQL connection was not created", connection instanceof ConnectionSource);
 
-        Dao<Tracer, Integer> therapyDao = DaoManager.createDao(connection, (Class) Tracer.class);
-        assertTrue("Did not create last table (tracers)", therapyDao.isTableExists());
-    }
+		Dao<Tracer, Integer> therapyDao = DaoManager.createDao(connection, (Class) Tracer.class);
+		assertTrue("Did not create last table (tracers)", therapyDao.isTableExists());
+	}
 
-    @AfterClass
-    public static void tearDownClass() {
-        TestSqlServerConnection.dropAllTables();
-    }
+	@AfterClass
+	public static void tearDownClass() {
+		TestSqlServerConnection.dropAllTables();
+	}
 
 }
--- src/main/java/nuclibooktest/test_utils/TestSqlServerConnection.java
+++ src/main/java/nuclibooktest/test_utils/TestSqlServerConnection.java
@@ -1,9 +1,7 @@
 package nuclibooktest.test_utils;
 
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
-import junit.framework.Test;
 import nuclibook.models.*;
 import nuclibook.server.SqlServerConnection;
 
@@ -11,38 +9,39 @@ import java.sql.SQLException;
 
 public class TestSqlServerConnection {
 
-    private static final String MYSQL_URI = "jdbc:mysql://bender.musalbas.com:3306/nuclibooktest";
-    private static final String MYSQL_USERNAME = "nuclibooktest";
-    private static final String MYSQL_PASSWORD = "qKRJ7Xj7MN2D3bV8";
+	private static final String MYSQL_URI = "jdbc:mysql://bender.musalbas.com:3306/nuclibooktest";
+	private static final String MYSQL_USERNAME = "nuclibooktest";
+	private static final String MYSQL_PASSWORD = "qKRJ7Xj7MN2D3bV8";
 
-    private TestSqlServerConnection() {
-    }
+	private TestSqlServerConnection() {
+	}
 
-    public static ConnectionSource acquireConnection() {
-        return SqlServerConnection.acquireConnection(MYSQL_URI, MYSQL_USERNAME, MYSQL_PASSWORD);
-    }
+	public static ConnectionSource acquireConnection() {
+		return SqlServerConnection.acquireConnection(MYSQL_URI, MYSQL_USERNAME, MYSQL_PASSWORD);
+	}
 
-    public static void dropAllTables() {
-        ConnectionSource connection = acquireConnection();
+	public static void dropAllTables() {
+		ConnectionSource connection = acquireConnection();
 
-        try {
-            TableUtils.dropTable(connection, ActionLog.class, true);
-            TableUtils.dropTable(connection, Booking.class, true);
-            TableUtils.dropTable(connection, BookingStaff.class, true);
-            TableUtils.dropTable(connection, Camera.class, true);
-            TableUtils.dropTable(connection, CameraType.class, true);
-            TableUtils.dropTable(connection, Patient.class, true);
-            TableUtils.dropTable(connection, Permission.class, true);
-            TableUtils.dropTable(connection, Staff.class, true);
-            TableUtils.dropTable(connection, StaffAbsence.class, true);
-            TableUtils.dropTable(connection, StaffAvailability.class, true);
-            TableUtils.dropTable(connection, StaffRole.class, true);
-            TableUtils.dropTable(connection, StaffRolePermission.class, true);
-            TableUtils.dropTable(connection, Therapy.class, true);
-            TableUtils.dropTable(connection, Tracer.class, true);
-        } catch (SQLException e) {
-            e.printStackTrace(); // TODO deal with exception
-        }
-    }
+		try {
+			TableUtils.dropTable(connection, ActionLog.class, true);
+			TableUtils.dropTable(connection, Booking.class, true);
+			TableUtils.dropTable(connection, BookingStaff.class, true);
+			TableUtils.dropTable(connection, Camera.class, true);
+			TableUtils.dropTable(connection, CameraType.class, true);
+			TableUtils.dropTable(connection, Patient.class, true);
+			TableUtils.dropTable(connection, Permission.class, true);
+			TableUtils.dropTable(connection, Staff.class, true);
+			TableUtils.dropTable(connection, StaffAbsence.class, true);
+			TableUtils.dropTable(connection, StaffAvailability.class, true);
+			TableUtils.dropTable(connection, StaffRole.class, true);
+			TableUtils.dropTable(connection, StaffRolePermission.class, true);
+			TableUtils.dropTable(connection, Therapy.class, true);
+			TableUtils.dropTable(connection, Tracer.class, true);
+		} catch (SQLException e) {
+			// TODO deal with exception
+			e.printStackTrace();
+		}
+	}
 
 }
