e2777f0d1d2bc430ae3811008ad2f24419651cd5
GEORGE RADUTA <k1332897@kcl.ac.uk>
2015-02-27 12:07:08 +0000

    Patients validation
    Upper and Lower limits set.
    
    Upper Limits no trim.

--- src/main/resources/static/js/patients.js
+++ src/main/resources/static/js/patients.js
@@ -1,15 +1,51 @@
 var validateEditForm = function (formObject) {
 	var error = false;
 
-	// TODO
+    // check name to be in interval [1,64]
+    if (formObject.name.trim().length < 1) {
+        toastr.error("You did not enter a valid name.");
+        error = true;
+    }
+    if (formObject.name.length > 64) {
+        toastr.error("Name of the patient should not exceed 64 characters");
+        error = true;
+    }
 
-	return !error;
+    // check hospital number to be in interval [1,64]
+    if (formObject["hospital-number"].trim().length < 1) {
+        toastr.error("You did not enter a valid hospital number.");
+        error = true;
+    }
+    if (formObject["hospital-number"].length > 64) {
+        toastr.error("Hospital Number should not exceed 64 characters");
+        error = true;
+    }
+
+    return !error;
 };
 
 var validateCreateForm = function (formObject) {
 	var error = false;
 
-	// TODO
+    // check name to be in interval [1,64]
+    if (formObject.name.trim().length < 1) {
+        toastr.error("You did not enter a valid name.");
+        error = true;
+    }
+    if (formObject.name.length > 64) {
+        toastr.error("Name of the patient should not exceed 64 characters");
+        error = true;
+    }
+
+    // check hospital number to be in interval [1,64]
+    if (formObject["hospital-number"].trim().length < 1) {
+        toastr.error("You did not enter a valid hospital number.");
+        error = true;
+    }
+    if (formObject["hospital-number"].length > 64) {
+        toastr.error("Hospital Number should not exceed 64 characters");
+        error = true;
+    }
 
 	return !error;
 };
