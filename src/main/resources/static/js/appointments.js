b7fe582c1f8a753b918c9a43956c471e01edad3a
NKParmarx <naviparmar73@gmail.com>
2015-02-27 12:20:16 +0000

    All slidey stuff working :) CSS still needs to be done

--- src/main/resources/static/js/appointments.js
+++ src/main/resources/static/js/appointments.js
@@ -1,41 +1,47 @@
+var patientId = 0;
+var therapyId = 0;
+
+
 $(document).ready(function () {
 
-    $('#select').click(function () {
+    $('#select-patient').click(function () {
+        // get id and name
+        patientId = $(this).attr('data-id');
+        $('.patient-selected').html($(this).attr('data-name'));
 
-        $('.page-one').slideUp(500);
-
-        $('#page-two').show();
-    })
-    $('#go-back-page-one').click(function () {
-        $('.page-one').show();
-
-        $('#page-two').hide();
-    })
-
-    $('#bookTherapy').click(function () {
-        $('#page-two').slideUp(500);
-
-        $('#page-three').show();
-    })
-
-    $('#selectTherapy').click(function () {
-        $('#page-three').slideUp(500);
-
-        $('#page-four').show();
-    })
-
-    $('#go-back-page-three').click(function () {
-
-        $('#page-three').show();
-
-        $('#page-four').hide();
-
+        // open next page
+        $('#page-one').slideUp(500);
+        $('#page-two').slideDown(500);
     });
 
+    $('#go-back-to-select-patient').click(function () {
+        // open prev page
+        $('#page-one').slideDown(500);
+        $('#page-two').slideUp(500);
+    });
 
-    $('#viewAvailableAppointments').click(function () {
-        $('#page-four').slideUp(500);
+    $('#select-therapy').click(function () {
+        // get id and name
+        therapyId = $(this).attr('data-id');
+        $('.therapy-selected').html($(this).attr('data-name'));
 
+        // open next page
+        $('#page-two').slideUp(500);
+        $('#page-three').slideDown(500);
+    });
+
+    $('#go-back-to-select-therapy').click(function () {
+        // open prev page
+        $('#page-two').slideDown(500);
+        $('#page-three').slideUp(500);
+    });
+
+    $('#view-available-appointments').click(function () {
+        // hide buttons
+        $(this).slideUp(300);
+        $('#go-back-to-select-therapy').hide();
+
+        // show calendar
         $('.calendar').show().fullCalendar({
             header: {
                 left: 'prev,next today',
@@ -43,9 +49,10 @@ $(document).ready(function () {
                 right: 'month,agendaWeek,agendaDay'
             },
             editable: true
-        })
+        });
     });
 
     // datatables
     setUpDataTable('#patients-table', 0, [[1, 1], [1, 1], [1, 1], [0, 0]]);
+    setUpDataTable('#therapies-table', 0, [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [0, 0]]);
 });
