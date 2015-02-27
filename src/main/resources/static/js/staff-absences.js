bf54701bf84875fbc9dc928d3ff1d0567e6496ac
Mark Ormesher <me@markormesher.co.uk>
2015-02-27 01:44:33 +0000

    Validation for staff availabilities

--- src/main/resources/static/js/staff-absences.js
+++ src/main/resources/static/js/staff-absences.js
@@ -1,13 +1,13 @@
 var validateCreateForm = function (formObject) {
-	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\ \\d{2}:\\d{2}');
-
 	var error = false;
+
+	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\ \\d{2}:\\d{2}');
 	if (!format.test(formObject['from'])) {
-		toastr.error("You did not enter the 'from' date in YYYY-MM-DD HH-MM format.");
+		toastr.error("You did not enter the 'from' date in YYYY-MM-DD HH:MM format.");
 		error = true;
 	}
 	if (!format.test(formObject['to'])) {
-		toastr.error("You did not enter the 'to' date in YYYY-MM-DD HH-MM format.");
+		toastr.error("You did not enter the 'to' date in YYYY-MM-DD HH:MM format.");
 		error = true;
 	}
 
@@ -15,15 +15,15 @@ var validateCreateForm = function (formObject) {
 };
 
 var validateEditForm = function (formObject) {
-	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\ \\d{2}:\\d{2}');
-
 	var error = false;
+
+	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\ \\d{2}:\\d{2}');
 	if (!format.test(formObject['from'])) {
-		toastr.error("You did not enter the 'from' date in YYYY-MM-DD HH-MM format.");
+		toastr.error("You did not enter the 'from' date in YYYY-MM-DD HH:MM format.");
 		error = true;
 	}
 	if (!format.test(formObject['to'])) {
-		toastr.error("You did not enter the 'to' date in YYYY-MM-DD HH-MM format.");
+		toastr.error("You did not enter the 'to' date in YYYY-MM-DD HH:MM format.");
 		error = true;
 	}
 
--- src/main/resources/static/js/staff-availabilities.js
+++ src/main/resources/static/js/staff-availabilities.js
@@ -1,7 +1,15 @@
 var validateCreateForm = function (formObject) {
 	var error = false;
 
-	// TODO
+	var format = new RegExp('\\d{2}:\\d{2}');
+	if (!format.test(formObject['start-time'])) {
+		toastr.error("You did not enter the start time in HH:MM format.");
+		error = true;
+	}
+	if (!format.test(formObject['end-time'])) {
+		toastr.error("You did not enter the end time in HH:MM format.");
+		error = true;
+	}
 
 	return !error;
 };
@@ -9,7 +17,15 @@ var validateCreateForm = function (formObject) {
 var validateEditForm = function (formObject) {
 	var error = false;
 
-	// TODO
+	var format = new RegExp('\\d{2}:\\d{2}');
+	if (!format.test(formObject['start-time'])) {
+		toastr.error("You did not enter the start time in HH:MM format.");
+		error = true;
+	}
+	if (!format.test(formObject['end-time'])) {
+		toastr.error("You did not enter the end time in HH:MM format.");
+		error = true;
+	}
 
 	return !error;
 };
