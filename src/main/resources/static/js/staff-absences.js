cc2dedace81b98b63689dbb8caab188410691f1c
Mark Ormesher <me@markormesher.co.uk>
2015-02-27 01:40:18 +0000

    Validation for staff absences

--- src/main/resources/static/js/staff-absences.js
+++ src/main/resources/static/js/staff-absences.js
@@ -1,15 +1,31 @@
 var validateCreateForm = function (formObject) {
-	var error = false;
+	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\ \\d{2}:\\d{2}');
 
-	// TODO
+	var error = false;
+	if (!format.test(formObject['from'])) {
+		toastr.error("You did not enter the 'from' date in YYYY-MM-DD HH-MM format.");
+		error = true;
+	}
+	if (!format.test(formObject['to'])) {
+		toastr.error("You did not enter the 'to' date in YYYY-MM-DD HH-MM format.");
+		error = true;
+	}
 
 	return !error;
 };
 
 var validateEditForm = function (formObject) {
-	var error = false;
+	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\ \\d{2}:\\d{2}');
 
-	// TODO
+	var error = false;
+	if (!format.test(formObject['from'])) {
+		toastr.error("You did not enter the 'from' date in YYYY-MM-DD HH-MM format.");
+		error = true;
+	}
+	if (!format.test(formObject['to'])) {
+		toastr.error("You did not enter the 'to' date in YYYY-MM-DD HH-MM format.");
+		error = true;
+	}
 
 	return !error;
 };
