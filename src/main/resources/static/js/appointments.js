4969c60fb59ec4d562f1ab6dac00f0217745f4d8
NKParmarx <naviparmar73@gmail.com>
2015-02-27 11:21:28 +0000

    Updated to new appointments page

--- src/main/resources/static/js/appointments.js
+++ src/main/resources/static/js/appointments.js
@@ -1,58 +1,57 @@
 var patientId = 0;
 var therapyId = 0;
 
-
 $(document).ready(function () {
 
-    $('#select-patient').click(function () {
-        // get id and name
-        patientId = $(this).attr('data-id');
-        $('.patient-selected').html($(this).attr('data-name'));
+	$('.select-patient').click(function () {
+		// get id and name
+		patientId = $(this).attr('data-id');
+		$('.patient-selected').html($(this).attr('data-name'));
 
-        // open next page
-        $('#page-one').slideUp(500);
-        $('#page-two').slideDown(500);
-    });
+		// open next page
+		$('#page-one').slideUp(500);
+		$('#page-two').slideDown(500);
+	});
 
-    $('#go-back-to-select-patient').click(function () {
-        // open prev page
-        $('#page-one').slideDown(500);
-        $('#page-two').slideUp(500);
-    });
+	$('#go-back-to-select-patient').click(function () {
+		// open prev page
+		$('#page-one').slideDown(500);
+		$('#page-two').slideUp(500);
+	});
 
-    $('#select-therapy').click(function () {
-        // get id and name
-        therapyId = $(this).attr('data-id');
-        $('.therapy-selected').html($(this).attr('data-name'));
+	$('.select-therapy').click(function () {
+		// get id and name
+		therapyId = $(this).attr('data-id');
+		$('.therapy-selected').html($(this).attr('data-name'));
 
-        // open next page
-        $('#page-two').slideUp(500);
-        $('#page-three').slideDown(500);
-    });
+		// open next page
+		$('#page-two').slideUp(500);
+		$('#page-three').slideDown(500);
+	});
 
-    $('#go-back-to-select-therapy').click(function () {
-        // open prev page
-        $('#page-two').slideDown(500);
-        $('#page-three').slideUp(500);
-    });
+	$('#go-back-to-select-therapy').click(function () {
+		// open prev page
+		$('#page-two').slideDown(500);
+		$('#page-three').slideUp(500);
+	});
 
-    $('#view-available-appointments').click(function () {
-        // hide buttons
-        $(this).slideUp(300);
-        $('#go-back-to-select-therapy').hide();
+	$('#view-available-appointments').click(function () {
+		// hide buttons
+		$(this).slideUp(300);
+		$('#go-back-to-select-therapy').hide();
 
-        // show calendar
-        $('.calendar').show().fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            editable: true
-        });
-    });
+		// show calendar
+		$('.calendar').show().fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			editable: true
+		});
+	});
 
-    // datatables
-    setUpDataTable('#patients-table', 0, [[1, 1], [1, 1], [1, 1], [0, 0]]);
-    setUpDataTable('#therapies-table', 0, [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [0, 0]]);
+	// datatables
+	setUpDataTable('#patients-table', 0, [[1, 1], [1, 1], [1, 1], [0, 0]]);
+	setUpDataTable('#therapies-table', 0, [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [0, 0]]);
 });
