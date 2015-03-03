e048574cb292d41ea44f120586357ba2d3eac2de
AinurMakhmet <ainur.makhmet@kcl.ac.uk>
2015-03-03 11:35:32 +0000

    Util class for PatientQuestions is created

---
+++ src/main/java/nuclibook/entity_utils/PatientQuestionsUtils.java
@@ -0,0 +1,39 @@
+package nuclibook.entity_utils;
+
+import nuclibook.models.BookingStaff;
+import nuclibook.models.PatientQuestions;
+import nuclibook.models.Therapy;
+
+import java.util.List;
+
+public class PatientQuestionsUtils extends AbstractEntityUtils {
+
+	public static PatientQuestions getPatientQuestions(String id) {
+		try {
+			return getPatientQuestions(Integer.parseInt(id));
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+
+	public static PatientQuestions getPatientQuestions(int id) {
+		return getEntityById(PatientQuestions.class, id);
+	}
+
+	public static List<PatientQuestions> getAllPatientQuestions() {
+		return getAllEntities(PatientQuestions.class);
+	}
+
+    public static List<PatientQuestions> getPatientQuestionsByTherapyId(String staffId) {
+        try {
+            return getPatientQuestionsByTherapyId(Integer.parseInt(staffId));
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static List<PatientQuestions> getPatientQuestionsByTherapyId(int therapyId) {
+        return getEntitiesByField(PatientQuestions.class, "therapy_id", therapyId);
+    }
+
+}
