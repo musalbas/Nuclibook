d26114a243928092e2c50855a4059f832d7709cd
Mustafa <mus@musalbas.com>
2015-03-03 15:56:57 +0000

    Add javadocs to ActionLog

--- src/main/java/nuclibook/models/ActionLog.java
+++ src/main/java/nuclibook/models/ActionLog.java
@@ -4,6 +4,9 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 import org.joda.time.DateTime;
 
+/**
+ * Model to represent an entry in the action log.
+ */
 @DatabaseTable(tableName = "action_log")
 public class ActionLog {
 
@@ -25,9 +28,19 @@ public class ActionLog {
 	@DatabaseField
 	private String note;
 
+	/**
+	 * Initialise action log with no fields.
+	 */
 	public ActionLog() {
 	}
 
+	/**
+	 * Initialise action log.
+	 * @param staff Staff who performed the action.
+	 * @param when Date the action was performed.
+	 * @param action The action (constant from entity_utils.ActionLogger).
+	 * @param associatedId The ID of the object that the action was performed on.
+	 */
 	public ActionLog(Staff staff, DateTime when, Integer action, Integer associatedId) {
 		this.staff = staff;
 		this.action = action;
@@ -35,50 +48,99 @@ public class ActionLog {
 		setWhen(when);
 	}
 
+	/**
+	 * Get the ID of the action.
+	 * @return The ID of the action.
+	 */
 	public Integer getId() {
 		return id;
 	}
 
+	/**
+	 * Set the ID of the action.
+	 * @param id The ID of the action.
+	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+	/**
+	 * Get the date the action was performed.
+	 * @return The date the action was performed.
+	 */
 	public DateTime getWhen() {
 		return new DateTime(when);
 	}
 
+	/**
+	 * Set the date the action was performed.
+	 * @param when The date the action was performed.
+	 */
 	public void setWhen(DateTime when) {
 		this.when = when.toString();
 	}
 
+	/**
+	 * Get the member of staff who performed the action.
+	 * @return The member of staff who performed the action.
+	 */
 	public Staff getStaff() {
 		return staff;
 	}
 
+	/**
+	 * Set the member of staff who performed the action.
+	 * @return The member of staff who performed the action.
+	 */
 	public void setStaff(Staff staff) {
 		this.staff = staff;
 	}
 
+	/**
+	 * Get the action that was performed.
+	 * @return The action that was performed.
+	 */
 	public Integer getAction() {
 		return action;
 	}
 
+
+	/**
+	 * Set the action that was performed.
+	 * @return The action that was performed.
+	 */
 	public void setAction(Integer action) {
 		this.action = action;
 	}
 
+	/**
+	 * Get the ID of the object that the action was performed on.
+	 * @return The ID of the object that the action was performed on.
+	 */
 	public Integer getAssociatedId() {
 		return associatedId;
 	}
 
+	/**
+	 * Set the ID of the object that the action was performed on.
+	 * @return The ID of the object that the action was performed on.
+	 */
 	public void setAssociatedId(Integer associatedId) {
 		this.associatedId = associatedId;
 	}
 
+	/**
+	 * Get the note associated with the action.
+	 * @return The note associated with the action.
+	 */
 	public String getNote() {
 		return note;
 	}
 
+	/**
+	 * Set the note associated with the action.
+	 * @return The note associated with the action.
+	 */
 	public void setNote(String note) {
 		this.note = note;
 	}
