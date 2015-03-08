6975f19614af29c9699a039b780a5d0ef560119d
Fares Alaboud <fares.alaboud@gmail.com>
2015-03-08 00:47:11 +0000

    cleaning up jquery code

--- src/main/resources/static/js/plugins/data-tables/styling.js
+++ src/main/resources/static/js/plugins/data-tables/styling.js
@@ -3,8 +3,10 @@
  */
 $(document).ready(function() {
 
-    $( ".dataTables_filter").children('label').eq(0).append( "<span class='input-group-addon'><i class='fa fa-search'></i></span>");
-    $( ".dataTables_filter" ).children('label').eq(0).wrapInner( "<div class='icon-field input-group' id='search-data-tables'></div>");
-    $( ".dataTables_filter" ).children('label').eq(0).children('input').eq(0).removeClass("input-small input-inline");
-    $( ".dataTables_filter" ).children('label').eq(0).unwrap();
+    var filterSearchBox = $( ".dataTables_filter").children('label').eq(0);
+    filterSearchBox.append( "<span class='input-group-addon'><i class='fa fa-search'></i></span>");
+    filterSearchBox.wrapInner( "<div class='icon-field input-group' id='search-data-tables'></div>");
+    filterSearchBox.children('input').eq(0).removeClass("input-small input-inline");
+    filterSearchBox.unwrap();
+
 });
