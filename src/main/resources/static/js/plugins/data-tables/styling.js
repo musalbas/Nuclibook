b9ecee08762f0be11fa80fc6231bf99c5c050e03
Fares Alaboud <fares.alaboud@gmail.com>
2015-03-08 14:33:14 +0000

    revert from coloured buttons to change button colours on hover

--- src/main/resources/static/js/plugins/data-tables/styling.js
+++ src/main/resources/static/js/plugins/data-tables/styling.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     var filterSearchBox = $( ".dataTables_filter").children('label').eq(0);
     filterSearchBox.append( "<span class='input-group-addon'><i class='fa fa-search'></i></span>");
-    filterSearchBox.wrapInner( "<div class='icon-field input-group' id='search-data-tables'></div>");
+    filterSearchBox.wrapInner( "<div class='icon-field input-group data-tables-icon-field' id='search-data-tables'></div>");
     filterSearchBox.children('input').eq(0).removeClass("input-small input-inline");
     filterSearchBox.unwrap();
 
