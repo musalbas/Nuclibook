5bff651a24299c0bf3239b8d8957fe3eb50fb740
Kristin <kristin.kasavetova@gmail.com>
2015-03-11 23:34:58 +0000

    changed json format to return a list with all bookings

--- src/main/java/nuclibook/routes/CalendarRoute.java
+++ src/main/java/nuclibook/routes/CalendarRoute.java
@@ -1,6 +1,7 @@
 package nuclibook.routes;
 
 import nuclibook.constants.P;
+import nuclibook.entity_utils.BookingUtils;
 import nuclibook.entity_utils.SecurityUtils;
 import nuclibook.models.Booking;
 import nuclibook.models.BookingSection;
@@ -8,8 +9,6 @@ import org.joda.time.DateTime;
 import spark.Request;
 import spark.Response;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class CalendarRoute extends DefaultRoute {
@@ -20,74 +19,50 @@ public class CalendarRoute extends DefaultRoute {
         prepareToHandle();
 
         // security check
-        //TODO - what will this be? It's both viewing and editing an appointment, since you can book from the calendar.
-
         if (!SecurityUtils.requirePermission(P.VIEW_APPOINTMENTS, response)) return null;
+
+        // get start/end date
         String start = request.queryParams("start");
         String end = request.queryParams("end");
-        DateTime startDate = new DateTime(start);
-        DateTime endDate = new DateTime(end);
+        DateTime startDate = new DateTime(start + " 00:00:00");
+        DateTime endDate = new DateTime(end + " 23:59:59");
 
-        List<DateTime> allDays = new ArrayList<DateTime>();
-        DateTime toAdd = startDate;
-        for (int i = startDate.getDayOfWeek(); i <= endDate.getDayOfWeek(); i++) {
-            allDays.add(toAdd);
-            toAdd = startDate.plusDays(1);
-        }
+        // get bookings between start/end dates
+        List<Booking> bookings = BookingUtils.getBookingsByDateRange(startDate, endDate);
 
-        //magix to retrieve bookings by date
-        //TODO: magician, pls provide ;(((
-        List<Booking> bookings = null;//BookingUtils.getBookingsByDateRange(startDate, endDate);
-
-        //TODO create a HashMap for [Day, List<Booking] pairs
-
+        //build json
         StringBuilder jsonOutput = new StringBuilder();
-        jsonOutput.append("{ 'week': [");
+        jsonOutput.append("{ 'bookings': [");
 
-        //put in bookings
-        for (int i = 0; i < allDays.size(); i++) {
-            jsonOutput.append("{");
-            jsonOutput.append("'day': '" + allDays.get(i) + "'");
-            jsonOutput.append("'bookings': [");
+        for (int i = 0; i < bookings.size(); i++) {
 
-            for (int j = 0; j < bookings.size(); j++) {
+            if (i == 0) {
                 jsonOutput.append("{");
-                jsonOutput.append("'patientId': '" + bookings.get(i).getPatient().getId() + "'");
-                jsonOutput.append("'therapyName': '" + bookings.get(i).getTherapy().getName() + "'");
-                jsonOutput.append("'bookingSections': [");
-
-                List<BookingSection> bookingSections = bookings.get(i).getBookingSections();
-                for (int k = 0; k < bookingSections.size(); k++) {
-                    jsonOutput.append("{");
-                    jsonOutput.append("'start-time': '" + bookingSections.get(i).getStart() + "'");
-                    jsonOutput.append("'end-time': '" + bookingSections.get(i).getEnd() + "'");
-
-                    if (j == bookingSections.size() - 1) {
-                        jsonOutput.append("}");
-                    } else {
-                        jsonOutput.append("}, ");
-                    }
-                }
-                //close booking sections array
-                jsonOutput.append("]");
-
-                if (j == bookings.size() - 1) {
-                    jsonOutput.append("}");
-                } else {
-                    jsonOutput.append("}, ");
-                }
-            }
-            //close bookings array
-            jsonOutput.append("]");
-
-            if (i == allDays.size() - 1) {
-                jsonOutput.append("}");
             } else {
-                jsonOutput.append("}, ");
+                jsonOutput.append(", { ");
             }
 
+            jsonOutput.append("'patientId': '").append(bookings.get(i).getPatient().getId() + "'");
+            jsonOutput.append("'therapyName': '").append(bookings.get(i).getTherapy().getName() + "'");
+            jsonOutput.append("'bookingSections': [");
+
+            List<BookingSection> bookingSections = bookings.get(i).getBookingSections();
+            for (int j = 0; j < bookingSections.size(); j++) {
+
+                if (j == 0) {
+                    jsonOutput.append("{");
+                } else {
+                    jsonOutput.append(", { ");
+                }
+
+                jsonOutput.append("'startTime': '").append(bookingSections.get(i).getStart() + "'");
+                jsonOutput.append("'endTime': '").append(bookingSections.get(i).getEnd() + "'");
+                jsonOutput.append("}");
+            }
+
+            jsonOutput.append("]}");
         }
-        //close days array and week immediately after
+
         jsonOutput.append("]}");
 
         return jsonOutput;
