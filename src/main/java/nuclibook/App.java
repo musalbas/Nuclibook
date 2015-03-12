87a2b56afe5c091a82d4350764949a4865a63205
danialsadiqmasood <danialsadiqmasood@hotmail.com>
2015-03-12 22:44:07 +0000

    Created a progress bar for the javafx stage

--- src/main/java/nuclibook/App.java
+++ src/main/java/nuclibook/App.java
@@ -1,50 +1,130 @@
 package nuclibook;
 
+import javafx.animation.Animation;
+import javafx.animation.Transition;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.util.Timer;
 
 public class App extends Application {
 
+    ToolBar toolBar1 = new ToolBar();
+    HBox hBox = new HBox(toolBar1);
+
     @Override
     public void start(Stage stage) {
-        WebView root = new WebView();
 
-        WebEngine webEngine = root.getEngine();
-
-        webEngine.load("http://localhost:4567");
-        stage.setScene(new Scene(root, 1132, 700));
-		stage.setMinHeight(340);
-		stage.setMinWidth(600);
+        final ProgressBar progressBar = new ProgressBar(0);
+        progressBar.prefWidthProperty().bind(toolBar1.widthProperty().subtract(30));
+        WebView webView = new WebView();
+        final WebEngine webEngine = webView.getEngine();
+        toolBar1.getItems().add(progressBar);
+        StackPane mainPane = new StackPane();
+        BorderPane pane = new BorderPane();
+        hBox.setHgrow(toolBar1, Priority.ALWAYS);
+        hBox.setPrefHeight(0);
+        hBox.setMinHeight(0);
+        pane.setBottom(hBox);
+        pane.setPickOnBounds(false);
+        mainPane.getChildren().add(webView);
+        mainPane.getChildren().add(pane);
+        Scene scene = new Scene(mainPane, 1132, 700);
+        stage.setScene(scene);
+        stage.setMinHeight(340);
+        stage.setMinWidth(600);
         stage.setMaximized(true);
         stage.show();
 
-        webEngine.getLoadWorker().stateProperty().addListener(
-            new ChangeListener<Worker.State>() {
-                public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
-                    if (newState == Worker.State.SUCCEEDED) {
-                        stage.setTitle(getTitle(webEngine));
-                    }
+
+        progressBar.progressProperty().bind(webEngine.getLoadWorker().progressProperty());
+        webView.getEngine().load("http://localhost:4567");
+        webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+            Timer timer =  new Timer();
+            Worker.State currentState;
+            boolean panelShown = false;
+            @Override
+            public void changed(ObservableValue<? extends Worker.State> value,
+                                Worker.State oldState, Worker.State newState) {
+
+                currentState = newState;
+                //only show progressbar if a second has passed and page has not loaded and if the progress value is less than 50%
+                timer.schedule(
+                        new java.util.TimerTask() {
+                            @Override
+                            public void run() {
+                                if (currentState == Worker.State.RUNNING && progressBar.getProgress() < 0.5) {
+                                    panelShown = true;
+                                    showPanel.play();
+                                }
+                            }
+                        }, 1000);
+
+                //hide progress bar if it has been shown after 2 seconds
+                if(currentState == Worker.State.SUCCEEDED && panelShown){
+                    timer.schedule(
+                            new java.util.TimerTask() {
+                                @Override
+                                public void run() {
+                                    panelShown = false;
+                                    hidePanel.play();
+                                }
+                            }, 2000);
+
+                }
+
+                //set pag
+                if(newState == Worker.State.SUCCEEDED){
+                    stage.setTitle(getTitle(webEngine));
                 }
             }
-        );
-
+        });
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    // From https://stackoverflow.com/questions/27820793/find-the-title-of-current-webpage-open-in-webengine-javafx
+
+    final Animation hidePanel = new Transition() {
+        {
+            setCycleDuration(Duration.millis(250));
+            setCycleCount(0);
+
+        }
+
+        @Override
+        protected void interpolate(double frac) {
+            final double size = 30 * (1.0 - frac);
+            hBox.setPrefHeight(size);
+        }
+    };
+
+    Animation showPanel = new Transition() {
+        {
+            setCycleDuration(Duration.millis(250));
+            setDelay(Duration.millis(1000));
+        }
+
+        @Override
+        protected void interpolate(double frac) {
+            final double size = 30 * frac;
+            hBox.setPrefHeight(size);
+        }
+    };
+
     private String getTitle(WebEngine webEngine) {
         Document doc = webEngine.getDocument();
         NodeList heads = doc.getElementsByTagName("head");
@@ -53,11 +133,10 @@ public class App extends Application {
             Element head = (Element)heads.item(0);
             NodeList titles = head.getElementsByTagName("title");
             if (titles.getLength() > 0) {
-                Node title = titles.item(0);
+                org.w3c.dom.Node title = titles.item(0);
                 titleText = title.getTextContent();
             }
         }
         return titleText ;
     }
-
 }
