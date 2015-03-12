44ea0893bf1b6162150b0a73e2ff09dfb631a9f7
Mark Ormesher <me@markormesher.co.uk>
2015-03-12 14:13:25 +0000

    Date selector fixed

--- src/main/java/nuclibook/server/LocalServer.java
+++ src/main/java/nuclibook/server/LocalServer.java
@@ -24,6 +24,8 @@ public class LocalServer {
 		Spark.staticFileLocation("/static");
 
 		// page security
+		SecurityUtils.attemptLogin("06111993", "123456789");
+
 		Spark.before((request, response) -> {
 			// check if they are accessing a non-secure page
 			String path = request.pathInfo();
