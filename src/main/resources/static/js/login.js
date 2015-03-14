6b721ac01c7696a977de38892078fbeeae61f439
Fares Alaboud <fares.alaboud@gmail.com>
2015-03-14 17:55:35 +0000

    caps lock indicator on login screen

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,6 +1,9 @@
 /**
  * Created by Fares Alaboud on 24/02/2015.
  */
+$(function () {
+    $("[rel='tooltip']").tooltip();
+});
 
 function keepFocus() {
     var focusedElement = $(stage == 2 ? '#password' : '#username');
@@ -11,6 +14,29 @@ function keepFocus() {
         setTimeout(function () {
             focusedElement.focus();
         }, 0);
+    });
+}
+
+function checkCapsLockStatus() {
+    $('[type=password]').keypress(function(e) {
+        var $password = $(this),
+            tooltipVisible = $('.tooltip').is(':visible'),
+            s = String.fromCharCode(e.which);
+
+        //Check if capslock is on. No easy way to test for this
+        //Tests if letter is upper case and the shift key is NOT pressed.
+        if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
+            if (!tooltipVisible)
+                $password.tooltip('show');
+        } else {
+            if (tooltipVisible)
+                $password.tooltip('hide');
+        }
+
+        //Hide the tooltip when moving away from the password field
+        $password.blur(function(e) {
+            $password.tooltip('hide');
+        });
     });
 }
 
