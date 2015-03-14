9d9693c510feb7eb12130ded2daa6f868099f664
Kristin <kristin.kasavetova@gmail.com>
2015-03-14 13:13:39 +0000

    createEntity now returns the entity object after it's created

--- src/main/java/nuclibook/entity_utils/AbstractEntityUtils.java
+++ src/main/java/nuclibook/entity_utils/AbstractEntityUtils.java
@@ -11,124 +11,127 @@ import java.util.List;
 
 public abstract class AbstractEntityUtils {
 
-	public static <E> List<E> getAllEntities(Class dbClass) {
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
-				return entityDao.queryForAll();
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-		return null;
-	}
+    public static <E> List<E> getAllEntities(Class dbClass) {
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
+                return entityDao.queryForAll();
+            } catch (SQLException e) {
+                // fail
+            }
+        }
+        return null;
+    }
 
-	public static <E> E getEntityById(Class dbClass, int id) {
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
-				E entity = entityDao.queryForId(id);
-				if (entity != null) return entity;
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-		return null;
-	}
+    public static <E> E getEntityById(Class dbClass, int id) {
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
+                E entity = entityDao.queryForId(id);
+                if (entity != null) return entity;
+            } catch (SQLException e) {
+                // fail
+            }
+        }
+        return null;
+    }
 
-	public static <E> List<E> getEntitiesByField(Class dbClass, String field, Object value) {
-		// set up server connection
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				// search for user
-				Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
-				List<E> entities = entityDao.queryForEq(field, value);
-				if (entities != null) {
-					return entities;
-				}
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-		return null;
-	}
+    public static <E> List<E> getEntitiesByField(Class dbClass, String field, Object value) {
+        // set up server connection
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                // search for user
+                Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
+                List<E> entities = entityDao.queryForEq(field, value);
+                if (entities != null) {
+                    return entities;
+                }
+            } catch (SQLException e) {
+                // fail
+            }
+        }
+        return null;
+    }
 
-	public static <E> void createEntity(Class dbClass, E entity) {
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
-				entityDao.create(entity);
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-	}
+    public static <E> E createEntity(Class dbClass, E entity) {
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
+                entityDao.create(entity);
+                return entity;
+            } catch (SQLException e) {
+                // fail
+                return null;
+            }
+        }
+        return null;
+    }
 
-	public static <E> void updateEntity(Class dbClass, E entity) {
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
-				entityDao.update(entity);
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-	}
+    public static <E> void updateEntity(Class dbClass, E entity) {
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
+                entityDao.update(entity);
+            } catch (SQLException e) {
+                // fail
+            }
+        }
+    }
 
-	public static <E> void deleteEntity(Class dbClass, E entity) {
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
-				entityDao.delete(entity);
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-	}
+    public static <E> void deleteEntity(Class dbClass, E entity) {
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
+                entityDao.delete(entity);
+            } catch (SQLException e) {
+                // fail
+            }
+        }
+    }
 
-	public static <E> void deleteEntityById(Class dbClass, int id) {
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
-				DeleteBuilder<E, Integer> deleteBuilder = entityDao.deleteBuilder();
-				deleteBuilder.where().idEq(id);
-				deleteBuilder.delete();
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-	}
+    public static <E> void deleteEntityById(Class dbClass, int id) {
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
+                DeleteBuilder<E, Integer> deleteBuilder = entityDao.deleteBuilder();
+                deleteBuilder.where().idEq(id);
+                deleteBuilder.delete();
+            } catch (SQLException e) {
+                // fail
+            }
+        }
+    }
 
-	public static <E> void deleteEntitiesByField(Class dbClass, String field, Object arg) {
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
-				DeleteBuilder<E, Integer> deleteBuilder = entityDao.deleteBuilder();
-				deleteBuilder.where().eq(field, arg);
-				deleteBuilder.delete();
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-	}
+    public static <E> void deleteEntitiesByField(Class dbClass, String field, Object arg) {
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                Dao<E, Integer> entityDao = DaoManager.createDao(conn, dbClass);
+                DeleteBuilder<E, Integer> deleteBuilder = entityDao.deleteBuilder();
+                deleteBuilder.where().eq(field, arg);
+                deleteBuilder.delete();
+            } catch (SQLException e) {
+                // fail
+            }
+        }
+    }
 
-	public static <E> Dao<E, Integer> acquireDao(Class dbClass) {
-		ConnectionSource conn = SqlServerConnection.acquireConnection();
-		if (conn != null) {
-			try {
-				return DaoManager.createDao(conn, dbClass);
-			} catch (SQLException e) {
-				// fail
-			}
-		}
-		return null;
-	}
+    public static <E> Dao<E, Integer> acquireDao(Class dbClass) {
+        ConnectionSource conn = SqlServerConnection.acquireConnection();
+        if (conn != null) {
+            try {
+                return DaoManager.createDao(conn, dbClass);
+            } catch (SQLException e) {
+                // fail
+            }
+        }
+        return null;
+    }
 }
