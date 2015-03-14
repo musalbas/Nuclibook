7ee0995f7821cbd190c383f6b4ee2e1f8f0d3108
Fares Alaboud <fares.alaboud@gmail.com>
2015-03-14 18:00:19 +0000

    caps-lock indicator on the staff creation modal on password fields, as well as changed "Account settings" on staff.html (line 118) to a direct link

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -73,3 +73,26 @@ function subMenuToggle(menuWrapper) {
 		menuWrapper.find('ul.drop-down-menu').hide();
 	}
 }
+
+// Check if capslock is on, show tooltip if it is
+function checkCapsLockStatus() {
+    $('[type=password]').keypress(function(e) {
+        var $password = $(this),
+            tooltipVisible = $('.tooltip').is(':visible'),
+            s = String.fromCharCode(e.which);
+
+        //Tests if letter is upper case and the shift key is NOT pressed.
+        if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
+            if (!tooltipVisible)
+                $password.tooltip('show');
+        } else {
+            if (tooltipVisible)
+                $password.tooltip('hide');
+        }
+
+        //Hide the tooltip when moving away from the password field
+        $password.blur(function(e) {
+            $password.tooltip('hide');
+        });
+    });
+}
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -14,13 +14,13 @@ function keepFocus() {
     });
 }
 
+// Check if capslock is on, show tooltip if it is
 function checkCapsLockStatus() {
     $('[type=password]').keypress(function(e) {
         var $password = $(this),
             tooltipVisible = $('.tooltip').is(':visible'),
             s = String.fromCharCode(e.which);
 
-        //Check if capslock is on. No easy way to test for this
         //Tests if letter is upper case and the shift key is NOT pressed.
         if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
             if (!tooltipVisible)
