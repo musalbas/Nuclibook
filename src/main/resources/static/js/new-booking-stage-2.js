04f9219e5b5086f415c2b85c85ae99a80bd00c76
Mark Ormesher <me@markormesher.co.uk>
2015-03-14 17:25:49 +0000

    Adding staff to booking (untested)

--- src/main/resources/static/js/new-booking-stage-2.js
+++ src/main/resources/static/js/new-booking-stage-2.js
@@ -1,3 +1,5 @@
+var assignedStaff = [];
+
 $(document).ready(function () {
 
 	// set the default tracer
@@ -16,4 +18,51 @@ $(document).ready(function () {
 		}
 	});
 
+	// set up staff adding
+	$('.add-staff-member').click(function (e) {
+		var selectedOption = $('#staff').find('option:selected');
+		var staffId = selectedOption.val();
+		var staffName = selectedOption.text();
+		addAssignedStaff(staffId, staffName);
+	});
 });
+
+function addAssignedStaff(staffId, staffName) {
+	// default?
+	if (staffId == 0) return;
+
+	// already in list?
+	for (var i in assignedStaff) {
+		if (assignedStaff[i] == staffId) return;
+	}
+	assignedStaff.push(staffId);
+
+	// add to output
+	var assignedStaffOutput = $('.assigned-staff');
+	assignedStaffOutput.html(
+		assignedStaffOutput.html()
+		+ "<span class=\"assigned-staff-" + staffId + "\">"
+		+ (assignedStaffOutput.html == "" ? "" : "<br />")
+		+ staffName
+		+ " <a class=\"remove-assigned-staff-" + staffId + "\" href=\"javascript:;\">[remove]</a>"
+		+ "</span>"
+	);
+
+	// set up removal button
+	$('.remove-assigned-staff-' + staffId).click(function (e) {
+		e.preventDefault();
+		removeAssignedStaff(staffId);
+	});
+}
+
+function removeAssignedStaff(staffId) {
+	// remove from HTML
+	$('span.assigned-staff-' + staffId).remove();
+
+	// remove from array
+	var newAssignedStaff = [];
+	for (var i in assignedStaff) {
+		if (assignedStaff[i] != staffId) newAssignedStaff.push(assignedStaff[i]);
+	}
+	assignedStaff = newAssignedStaff;
+}
