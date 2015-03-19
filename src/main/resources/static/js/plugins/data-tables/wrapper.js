e41056027c50f421a31bd421c1a16b50efbd5830
Mark Ormesher <me@markormesher.co.uk>
2015-03-19 13:15:26 +0000

    Fade on load

--- src/main/resources/static/js/plugins/data-tables/wrapper.js
+++ src/main/resources/static/js/plugins/data-tables/wrapper.js
@@ -19,41 +19,51 @@ function setUpDataTable(tableId, colDefs, extraConfig) {
 		var nativeAlert = window.alert;
 		return function (message) {
 			window.alert = nativeAlert;
-			message.indexOf("DataTables warning") === 0 ?
+			message != null && message.indexOf("DataTables warning") === 0 ?
 				console.warn(message) :
 				nativeAlert(message);
 		}
 	})();
 
 	// do the magic
-	$(tableId).DataTable(mergeObject({
-		"language": {
-			"aria": {
-				"sortAscending": ": activate to sort column ascending",
-				"sortDescending": ": activate to sort column descending"
+	$(tableId)
+		.DataTable(mergeObject({
+			"language": {
+				"aria": {
+					"sortAscending": ": activate to sort column ascending",
+					"sortDescending": ": activate to sort column descending"
+				},
+				"emptyTable": "<em>There is no data to display in this table.</em>",
+				"info": "Showing _START_ of _END_ (_TOTAL_ results found)",
+				"infoEmpty": "No entries found",
+				"infoFiltered": "from _MAX_ entries",
+				"lengthMenu": "Display _MENU_ rows",
+				"search": "",
+				"zeroRecords": "<em>We couldn't find what you were searching for. Please check your spelling or try searching manually.</em>",
+				"paginate": {
+					"previous": "Prev",
+					"next": "Next",
+					"last": "Last",
+					"first": "First"
+				}
 			},
-			"emptyTable": "<i>There is no data to display in this table.</i>",
-			"info": "Showing _START_ of _END_ (_TOTAL_ results found)",
-			"infoEmpty": "No entries found",
-			"infoFiltered": "from _MAX_ entries",
-			"lengthMenu": "Display _MENU_ rows",
-			"search": "",
-			"zeroRecords": "<i>We couldn't find what you were searching for. Please check your spelling or try searching manually.</i>",
-			"paginate": {
-				"previous": "Prev",
-				"next": "Next",
-				"last": "Last",
-				"first": "First"
-			}
-		},
-		"bStateSave": false,
-		"columns": columnDefs,
-		"lengthMenu": [
-			[20, 50, -1],
-			[20, 50, "All"]
-		],
-		"pageLength": 20,
-		"pagingType": "bootstrap_full_number"
-	}, extraConfig));
+			"bStateSave": false,
+			"columns": columnDefs,
+			"lengthMenu": [
+				[20, 50, -1],
+				[20, 50, "All"]
+			],
+			"pageLength": 20,
+			"pagingType": "bootstrap_full_number"
+		}, extraConfig))
+		.on('preXhr.dt', function (e, s, j) {
+			$(this).fadeTo(0, 0.4);
+		})
+		.on('xhr.dt', function(e, s, j) {
+			$(this).fadeTo(0, 1.0);
+		});
+
+	// kill processing message
+	$('.dataTables_processing').remove();
 
 }
