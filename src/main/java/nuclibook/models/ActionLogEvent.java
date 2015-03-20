30ecfa0d9937d308f48fca2d01755248022cc744
Fares Alaboud <fares.alaboud@gmail.com>
2015-03-20 21:45:08 +0000

    Added JavaDoc for ActionLogEvent (complete) and BookingPatternSection (incomplete, see TODOs)

--- src/main/java/nuclibook/models/ActionLogEvent.java
+++ src/main/java/nuclibook/models/ActionLogEvent.java
@@ -18,25 +18,48 @@ public class ActionLogEvent {
 	@DatabaseField(columnName = "is_error")
 	private boolean isError;
 
+    /**
+     * Initialise the action log event with no fields.
+     */
 	public ActionLogEvent() {
 	}
 
+    /**
+     * Get the ID of the event.
+     * @return the ID of the event.
+     */
 	public Integer getId() {
 		return id;
 	}
 
+    /**
+     * Get the label of the event.
+     * @return the label of the event.
+     */
 	public String getLabel() {
 		return label;
 	}
 
+    /**
+     * Set the label of the event.
+     * @param label The label of the event.
+     */
 	public void setLabel(String label) {
 		this.label = label;
 	}
 
+    /**
+     * Check if it's an error or not.
+     * @return whether it's an error.
+     */
 	public boolean isError() {
 		return isError;
 	}
 
+    /**
+     * Set if it's an error or not.
+     * @param isError whether it's an error.
+     */
 	public void setError(boolean isError) {
 		this.isError = isError;
 	}
--- src/main/java/nuclibook/models/BookingPatternSection.java
+++ src/main/java/nuclibook/models/BookingPatternSection.java
@@ -3,6 +3,9 @@ package nuclibook.models;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+/**
+ * Model to represent a section of the pattern for a booking.
+ */
 @DatabaseTable(tableName = "booking_pattern_sections")
 public class BookingPatternSection {
 
@@ -24,53 +27,98 @@ public class BookingPatternSection {
 	@DatabaseField
 	private int sequence;
 
+    /**
+     * Initialises a pattern section without the fields.
+     */
 	public BookingPatternSection() {
 	}
 
+    /**
+     * Get the ID of the booking pattern section.
+     * @return the ID of the booking pattern section.
+     */
 	public Integer getId() {
 		return id;
 	}
 
+    /**
+     * Set the ID of the booking pattern section.
+     * @param id the ID of the booking pattern section.
+     */
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+    /**
+     * Get the therapy which this pattern's booking represents.
+     * @return the therapy which this pattern's booking represents.
+     */
 	public Therapy getTherapy() {
 		return therapy;
 	}
 
+    /**
+     * Set the therapy which this pattern's booking represents.
+     * @param therapy the therapy which this pattern's booking represents.
+     */
 	public void setTherapy(Therapy therapy) {
 		this.therapy = therapy;
 	}
 
+    /**
+     * Checks if the section is busy or free.
+     * @return whether the section is busy or free.
+     */
 	public boolean isBusy() {
 		return busy;
 	}
 
+    /**
+     * Sets whether the section is busy or free.
+     * @param busy whether the section is busy or free.
+     */
 	public void setBusy(boolean busy) {
 		this.busy = busy;
 	}
 
+    /**
+     * Gets the minimum length (in minutes) of a section.
+     * @return the minimum length (in minutes) of a section.
+     */
 	public int getMinLength() {
 		return minLength;
 	}
 
+    /**
+     * Sets the minimum the minimum length (in minutes) of a section.
+     * @param minLength the minimum length (in minutes) of a section.
+     */
 	public void setMinLength(int minLength) {
 		this.minLength = minLength;
 	}
 
-	public int getMaxLength() {
+    /**
+     * Gets the maximum length (in minutes) of a section.
+     * @return the maximum length (in minutes) of a section.
+     */
+    public int getMaxLength() {
 		return maxLength;
 	}
 
-	public void setMaxLength(int maxLength) {
+    /**
+     * Sets the minimum the maximum length (in minutes) of a section.
+     * @param maxLength the maximum length (in minutes) of a section.
+     */
+    public void setMaxLength(int maxLength) {
 		this.maxLength = maxLength;
 	}
 
+    //TODO: Unsure of JavaDoc
 	public int getSequence() {
 		return sequence;
 	}
 
+    //TODO: Unsure of JavaDoc
 	public void setSequence(int sequence) {
 		this.sequence = sequence;
 	}
