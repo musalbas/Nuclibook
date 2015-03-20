7b5a30e823ac0c6273aad8eb6c2bd7c5b72c0311
Kristin <kristin.kasavetova@gmail.com>
2015-03-20 09:48:14 +0000

    created js for generic events html page

---
+++ src/main/resources/static/js/generic-events.js
@@ -0,0 +1,47 @@
+var validateCreateForm = function (formObject) {
+	var error = false;
+
+	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\');
+	if (!format.test(formObject['from'])) {
+		toastr.error("You did not enter the 'from' date in YYYY-MM-DD format.");
+		error = true;
+	}
+	if (!format.test(formObject['to'])) {
+		toastr.error("You did not enter the 'to' date in YYYY-MM-DD format.");
+		error = true;
+	}
+
+	if (new Date(formObject['from']) >= new Date(formObject['to'])) {
+		toastr.error("The 'from' date cannot be after the 'to' date.");
+		error = true;
+	}
+
+	return !error;
+};
+
+var validateEditForm = function (formObject) {
+	var error = false;
+
+	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\');
+	if (!format.test(formObject['from'])) {
+		toastr.error("You did not enter the 'from' date in YYYY-MM-DD format.");
+		error = true;
+	}
+	if (!format.test(formObject['to'])) {
+		toastr.error("You did not enter the 'to' date in YYYY-MM-DD format.");
+		error = true;
+	}
+
+	if (new Date(formObject['from']) >= new Date(formObject['to'])) {
+		toastr.error("The 'from' date cannot be after the 'to' date.");
+		error = true;
+	}
+
+	return !error;
+};
+
+$(document).ready(function () {
+	setUpDataTable('#generic-events', [[1, 1], [1, 1], [0, 0]], {
+		order: [0, 'asc']
+	});
+});
