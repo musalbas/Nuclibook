e6772d7e0f08d22123701a0381888c37359e7cc0
Kristin <kristin.kasavetova@gmail.com>
2015-03-20 14:00:39 +0000

    js fixes  for date-time selectors

--- src/main/resources/static/js/generic-events.js
+++ src/main/resources/static/js/generic-events.js
@@ -1,47 +1,52 @@
 var validateCreateForm = function (formObject) {
-	var error = false;
+    var error = false;
 
-	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\');
-	if (!format.test(formObject['from'])) {
-		toastr.error("You did not enter the 'from' date in YYYY-MM-DD format.");
-		error = true;
-	}
-	if (!format.test(formObject['to'])) {
-		toastr.error("You did not enter the 'to' date in YYYY-MM-DD format.");
-		error = true;
-	}
+    var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}');
+    if (!format.test(formObject['from-date'])) {
+        toastr.error("You did not enter the 'from' date in YYYY-MM-DD format.");
+        error = true;
+    }
+    if (!format.test(formObject['to-date'])) {
+        toastr.error("You did not enter the 'to' date in YYYY-MM-DD format.");
+        error = true;
+    }
 
-	if (new Date(formObject['from']) >= new Date(formObject['to'])) {
-		toastr.error("The 'from' date cannot be after the 'to' date.");
-		error = true;
-	}
+    if (new Date(formObject['from-date'] + " " + formObject['from-time']) >= new Date(formObject['to-date'] + " " + formObject['to-time'])) {
+        toastr.error("The 'from' date cannot be after the 'to' date.");
+        error = true;
+    }
 
-	return !error;
+    return !error;
 };
 
 var validateEditForm = function (formObject) {
-	var error = false;
+    var error = false;
 
-	var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}\\');
-	if (!format.test(formObject['from'])) {
-		toastr.error("You did not enter the 'from' date in YYYY-MM-DD format.");
-		error = true;
-	}
-	if (!format.test(formObject['to'])) {
-		toastr.error("You did not enter the 'to' date in YYYY-MM-DD format.");
-		error = true;
-	}
+    var format = new RegExp('\\d{4}\\-\\d{2}\\-\\d{2}');
+    if (!format.test(formObject['from-date'])) {
+        toastr.error("You did not enter the 'from' date in YYYY-MM-DD format.");
+        error = true;
+    }
+    if (!format.test(formObject['to-date'])) {
+        toastr.error("You did not enter the 'to' date in YYYY-MM-DD format.");
+        error = true;
+    }
 
-	if (new Date(formObject['from']) >= new Date(formObject['to'])) {
-		toastr.error("The 'from' date cannot be after the 'to' date.");
-		error = true;
-	}
+    if (new Date(formObject['from-date'] + " " + formObject['from-time']) >= new Date(formObject['to-date'] + " " + formObject['to-time'])) {
+        toastr.error("The 'from' date cannot be after the 'to' date.");
+        error = true;
+    }
 
-	return !error;
+    return !error;
+};
+
+var onFormLoadSetup = function () {
+    prepareDateSelector();
+    prepareTimeSelector();
 };
 
 $(document).ready(function () {
-	setUpDataTable('#generic-events', [[1, 1], [1, 1], [0, 0]], {
-		order: [0, 'asc']
-	});
+    setUpDataTable('#generic-events', [[1, 1], [1, 1], [0, 0]], {
+        order: [0, 'asc']
+    });
 });
