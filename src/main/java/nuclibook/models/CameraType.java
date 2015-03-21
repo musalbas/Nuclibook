6e141def4014086d8ffe0646fe8ac756bb00fb2b
Kristin <kristin.kasavetova@gmail.com>
2015-03-21 20:16:05 +0000

    redundant constructor and code reformat

--- src/main/java/nuclibook/models/CameraType.java
+++ src/main/java/nuclibook/models/CameraType.java
@@ -12,74 +12,74 @@ import java.util.HashMap;
 @DatabaseTable(tableName = "camera_types")
 public class CameraType implements Renderable {
 
-	@DatabaseField(generatedId = true)
-	private Integer id;
+    @DatabaseField(generatedId = true)
+    private Integer id;
 
-	@DatabaseField(width = 64)
-	private String label;
+    @DatabaseField(width = 64)
+    private String label;
 
-	@DatabaseField(defaultValue = "true")
-	private Boolean enabled;
-
-    /**
-     * Initialise a camera type without fields.
-     */
-	public CameraType() {
-	}
+    @DatabaseField(defaultValue = "true")
+    private Boolean enabled;
 
     /**
      * Gets the ID of the camera type.
+     *
      * @return the ID of the camera type.
      */
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
     /**
      * Sets the ID of the camera type.
+     *
      * @param id the ID of the camera type.
      */
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     /**
      * Gets the label of the camera type.
+     *
      * @return the label of the camera type.
      */
-	public String getLabel() {
-		return label;
-	}
+    public String getLabel() {
+        return label;
+    }
 
     /**
      * Sets the label of the camera type.
+     *
      * @param label the label of the camera type.
      */
-	public void setLabel(String label) {
-		this.label = label;
-	}
+    public void setLabel(String label) {
+        this.label = label;
+    }
 
     /**
      * Gets whether it's enabled or not.
+     *
      * @return whether it's enabled or not.
      */
-	public Boolean getEnabled() {
-		return enabled;
-	}
+    public Boolean getEnabled() {
+        return enabled;
+    }
 
     /**
      * Sets whether it's enabled or not.
+     *
      * @param enabled whether it's enabled or not.
      */
-	public void setEnabled(Boolean enabled) {
-		this.enabled = enabled;
-	}
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
 
-	@Override
-	public HashMap<String, String> getHashMap() {
-		return new HashMap<String, String>(){{
-			put("id", getId().toString());
-			put("label", getLabel());
-		}};
-	}
+    @Override
+    public HashMap<String, String> getHashMap() {
+        return new HashMap<String, String>() {{
+            put("id", getId().toString());
+            put("label", getLabel());
+        }};
+    }
 }
