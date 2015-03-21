cc247cebb02cce3774e8dd5d7cc3c73e5896403b
Kristin <kristin.kasavetova@gmail.com>
2015-03-21 19:57:44 +0000

    redundant constructor and reformat

--- src/main/java/nuclibook/models/TherapyCameraType.java
+++ src/main/java/nuclibook/models/TherapyCameraType.java
@@ -9,76 +9,68 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable(tableName = "therapy_camera")
 public class TherapyCameraType {
 
-	@DatabaseField(generatedId = true)
-	private Integer id;
+    @DatabaseField(generatedId = true)
+    private Integer id;
 
-	@DatabaseField(canBeNull = false, foreign = true, columnName = "therapy_id", foreignAutoRefresh = true)
-	private Therapy therapy;
+    @DatabaseField(canBeNull = false, foreign = true, columnName = "therapy_id", foreignAutoRefresh = true)
+    private Therapy therapy;
 
-	@DatabaseField(canBeNull = false, foreign = true, columnName = "camera_type_id", foreignAutoRefresh = true)
-	private CameraType cameraType;
-
-    /**
-     * Initialise a relationship without the fields.
-     */
-	public TherapyCameraType() {
-	}
+    @DatabaseField(canBeNull = false, foreign = true, columnName = "camera_type_id", foreignAutoRefresh = true)
+    private CameraType cameraType;
 
     /**
      * Initialise a relationship with a therapy and a camera type.
-     * @param therapy the therapy object
+     *
+     * @param therapy    the therapy object
      * @param cameraType the camera type object
      */
-	public TherapyCameraType(Therapy therapy, CameraType cameraType) {
-		this.therapy = therapy;
-		this.cameraType = cameraType;
-	}
+    public TherapyCameraType(Therapy therapy, CameraType cameraType) {
+        this.therapy = therapy;
+        this.cameraType = cameraType;
+    }
 
     /**
      * Gets the ID of the relationship.
+     *
      * @return the ID of the relationship.
      */
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
     /**
      * Sets the ID of the relationship
+     *
      * @param id the ID of the relationship
      */
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     /**
      * Gets the camera type.
+     *
      * @return the camera type.
      */
-	public CameraType getCameraType() {
-		return cameraType;
-	}
-
-    /**
-     * Sets the camera type.
-     * @param cameraType the camera type.
-     */
-	public void setCameraType(CameraType cameraType) {
-		this.cameraType = cameraType;
-	}
+    public CameraType getCameraType() {
+        return cameraType;
+    }
 
     /**
      * Gets the therapy.
+     *
      * @return the therapy.
      */
-	public Therapy getTherapy() {
-		return therapy;
-	}
+    public Therapy getTherapy() {
+        return therapy;
+    }
 
     /**
      * Sets the therapy.
+     *
      * @param therapy the therapy.
      */
-	public void setTherapy(Therapy therapy) {
-		this.therapy = therapy;
-	}
+    public void setTherapy(Therapy therapy) {
+        this.therapy = therapy;
+    }
 }
