9369a1b40a34df945edd2fb56e618f0d8a4ac6d3
Mark Ormesher <me@markormesher.co.uk>
2015-03-23 09:42:43 +0000

    JavaDoc finished on Renderable

--- src/main/java/nuclibook/server/Renderable.java
+++ src/main/java/nuclibook/server/Renderable.java
@@ -2,17 +2,13 @@ package nuclibook.server;
 
 import java.util.HashMap;
 
+/**
+ * This interface must be implemented by any object to be rendered by {@link nuclibook.server.HtmlRenderer}
+ */
 public interface Renderable {
 
 	/**
-	 * This interface must be implemented by any object to be rendered by HtmlRenderer.
-	 */
-
-	/**
-	 * @return A hashmap of all possible fields that could be called in the front-end
-	 *         HTML template. If the front-end will call <!--[field: status]--> there must
-	 *         be an entry in this map, ["status", "..."]. An missing or null values will
-	 *         be rendered as "".
+	 * @return A HashMap of all possible fields that could be called in the front-end HTML template.
 	 */
 	public HashMap<String, String> getHashMap();
 
